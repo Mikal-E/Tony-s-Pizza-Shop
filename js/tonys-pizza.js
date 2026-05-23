334074612cf5643ea4a7e3b12938999b28f0719c
Mikal-E <melamin101@gmail.com>
2026-05-22 21:18:45 -0400

    Comparison Operators - completed Boolean exercises and explored the floating point precision quirk caused by storing of decimal numbers in binary.

--- js/tonys-pizza.js
+++ js/tonys-pizza.js
@@ -7,14 +7,18 @@ let isAvailable = true;               - can change based on inventory
 
 */
 
+// TOPIC - DATA TYPES
+
 // Strings - text values
 
 let pizzaName = "Sicilian";
+console.log(pizzaName);
+
 const shopName = "Tony's Pizza Shop";
 console.log(shopName);
 
-shopName = "Mario's Pizza";
-console.log(shopName);
+// shopName = "Mario's Pizza";
+// console.log(shopName);
 
 // Numbers
 
@@ -24,20 +28,49 @@ console.log(pizzaPrice);
 pizzaPrice = 21.99;
 console.log(pizzaPrice);
 
-
 // Boolean
 
 let isAvailable = true;
+console.log(isAvailable);
 
 // Null - intentionally empty
 
 let currentOrder = null;
+console.log(currentOrder);
 
 // Undefined - declared but not assigned
 
 let deliveryDriver;
-
-console.log(pizzaName);
-console.log(isAvailable);
-console.log(currentOrder);
 console.log(deliveryDriver);
+
+
+// TOPIC - OPERATORS
+
+let price = 18.99;
+let discount = 5;
+
+console.log(price + discount);
+console.log(price - discount);
+console.log(price * 2);
+console.log(price / 2);
+console.log(price % 2);
+
+// Modulus
+
+console.log(10 % 2); // Even, no remainder
+console.log(11 % 2); // Odd, has a remainder
+
+// Comparison Operators
+
+console.log(pizzaPrice > 15);
+console.log(pizzaPrice < 15);
+console.log(pizzaPrice >= 18.99);
+console.log(pizzaPrice === 18.99); // Came out false.
+console.log(pizzaPrice !== 20);
+
+console.log(18.99 === 18.99);  // Came out true here.
+console.log(0.1 + 0.2 === 0.3); // Came out false.
+console.log(0.1 + 0.2); // Returned 0.30000000000000004.
+
+console.log(18 == "18");
+console.log(18 === "18");
